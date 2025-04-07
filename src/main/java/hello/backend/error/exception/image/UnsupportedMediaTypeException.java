bfb4182bb097e9f1a11fc5df2110d23e765c6988
kittae <jinkitea@naver.com>
2025-04-07 23:41:47 +0900

    refactor: 경로 수정

--- src/main/java/hello/backend/error/exception/image/UnsupportedMediaTypeException.java
+++ src/main/java/hello/backend/error/exception/image/UnsupportedMediaTypeException.java
@@ -1,4 +1,4 @@
-package hello.backend.exception;
+package hello.backend.error.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
