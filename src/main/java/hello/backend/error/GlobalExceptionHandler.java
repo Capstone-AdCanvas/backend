8cca35048d3db2610a06a2c47bc75d4234b38dff
kittae <jinkitea@naver.com>
2025-04-12 17:11:49 +0900

    refactor: 예외처리 관련 세팅 및 리팩토링

--- src/main/java/hello/backend/error/GlobalExceptionHandler.java
+++ src/main/java/hello/backend/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package hello.backend;
+package hello.backend.error;
 
-import hello.backend.error.ErrorResponse;
 import hello.backend.error.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
