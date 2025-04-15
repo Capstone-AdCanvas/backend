b9819c898a335cabb6e033c6f37b8f81875ac48e
kittae <jinkitea@naver.com>
2025-04-15 19:37:06 +0900

    refactor: Video 비동기 큐 방식으로 리팩토링

--- src/main/java/hello/backend/video/dto/SubmitQueueResponse.java
+++ src/main/java/hello/backend/video/dto/SubmitQueueResponse.java
@@ -1,4 +1,13 @@
 package hello.backend.video.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class SubmitQueueResponse {
+    String requestId;
+    String message;
 }
