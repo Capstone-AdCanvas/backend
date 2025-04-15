b00c3da49c2403427e5db59bf92f41116aa40707
kittae <jinkitea@naver.com>
2025-04-15 19:36:01 +0900

    chore: Caffeine 설정 추가

--- src/main/java/hello/backend/config/CaffeineConfig.java
+++ src/main/java/hello/backend/config/CaffeineConfig.java
@@ -1,4 +1,30 @@
 package hello.backend.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import hello.backend.video.dto.ImageToVideoRequest;
+import hello.backend.video.dto.TextToVideoRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
 public class CaffeineConfig {
+
+    @Bean
+    public Cache<String, TextToVideoRequest> requestTextCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(1000)
+                .build();
+    }
+
+    @Bean
+    public Cache<String, ImageToVideoRequest> requestImageCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(1000)
+                .build();
+    }
 }
