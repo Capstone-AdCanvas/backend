3160233231d6962a52d7ba21fa3f9481e44dc5b0
kittae <jinkitea@naver.com>
2025-05-07 23:18:05 +0900

    chore: ffmpeg 설정 추가

--- src/main/java/hello/backend/config/FFmpegProperties.java
+++ src/main/java/hello/backend/config/FFmpegProperties.java
@@ -2,9 +2,7 @@ package hello.backend.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +10,9 @@ import org.springframework.context.annotation.Configuration;
 @Getter @Setter
 @Configuration
 @ConfigurationProperties(prefix = "ffmpeg")
-public class FFmpegConfig {
+public class FFmpegProperties {
 
     private String mpeg;
     private String probe;
     private String savePath;
-
 }
