99d3a9c1c0af70e5a48c2ef5e92fd385668b03e0
kittae <jinkitea@naver.com>
2025-05-07 23:18:05 +0900

    chore: ffmpeg 설정 추가

--- src/main/java/hello/backend/config/FFmpegConfig.java
+++ src/main/java/hello/backend/config/FFmpegConfig.java
@@ -1,4 +1,42 @@
 package hello.backend.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
 public class FFmpegConfig {
+
+    private final FFmpegProperties fFmpegProperties;
+    private static String SAVE_PATH;
+
+    @PostConstruct
+    public void init() {
+        SAVE_PATH = fFmpegProperties.getSavePath();
+        log.info("[FFmpegConfig] Save path initialized to: " + SAVE_PATH);
+    }
+
+    @Bean
+    public FFmpeg ffmpeg() throws IOException {
+        return new FFmpeg(fFmpegProperties.getMpeg());
+    }
+
+    @Bean
+    public FFprobe ffprobe() throws IOException {
+        return new FFprobe(fFmpegProperties.getProbe());
+    }
+
+    @Bean
+    public FFmpegExecutor ffmpegExecutor(FFmpeg ffmpeg, FFprobe ffprobe) {
+        return new FFmpegExecutor(ffmpeg, ffprobe);
+    }
 }
