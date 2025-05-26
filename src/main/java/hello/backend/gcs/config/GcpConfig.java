83ab49de131f6263990dd228daf896b44bfe9e7b
kittae <jinkitea@naver.com>
2025-05-26 23:15:59 +0900

    refactoring: gcs 경로 리팩토링

--- src/main/java/hello/backend/gcs/config/GcpConfig.java
+++ src/main/java/hello/backend/gcs/config/GcpConfig.java
@@ -3,22 +3,27 @@ package hello.backend.gcs.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 
 @Configuration
 public class GcpConfig {
 
+    @Value("${gcp.credentials}")
+    private String credentialsPath;
+
     @Bean
     public Storage storage() throws IOException {
-        // resources/gcs-service-key.json 을 classpath에서 불러옴
-        ClassPathResource resource = new ClassPathResource("project-adcanvas-7198154f5844.json");
+        File file = new File(credentialsPath);
 
-        try (InputStream inputStream = resource.getInputStream()) {
+        try (InputStream inputStream = Files.newInputStream(file.toPath())) {
             return StorageOptions.newBuilder()
                     .setCredentials(GoogleCredentials.fromStream(inputStream))
                     .build()
